e8771e2f0a00e66e9de6e90407c18b663eb98e46
Cedric <Cedric@DESKTOP-BTF3HDL>
2018-07-17 09:46:31 +0200

    Changed default layout

--- resources/views/vendor/mail/html/footer.blade.php
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -3,6 +3,15 @@
         <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0">
             <tr>
                 <td class="content-cell" align="center">
+                    <style>
+                       #icon{
+                          width: 32px;
+                       }
+                    </style>
+                    <p>Vind ons ook op sociale media</p>
+                    <a href="https://www.facebook.com/Prismacare-225274354778984/"><img id="icon" src="https://cdn4.iconfinder.com/data/icons/social-media-icons-the-circle-set/48/facebook_circle-512.png" alt=""></a>
+                    <a href="https://www.instagram.com/prisma.care/"><img id="icon"src="https://cdn2.iconfinder.com/data/icons/instagram-new/512/instagram-round-3d-512.png" alt=""></a>
+
                     {{ Illuminate\Mail\Markdown::parse($slot) }}
                 </td>
             </tr>
--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,5 +1,5 @@
 <tr>
     <td class="header">
-        <a href="{{ $url }}"><img src="https://prisma.care/images/logo-for-emails.png"></a>
+        <a href="{{ $url }}"><img width="100" src="https://files.slack.com/files-tmb/T8EPNSSF4-FBHLFG2HY-5ca03d6149/logo_prisma_2_960.png"></a>
     </td>
 </tr>
